ffe7c170357b020a50d55556fb69e1a0657355a5
Hope-Kerubo <kerubohopes@mail.com>
2023-08-30 11:47:04 +0300

    added Expense class

---
+++ Expense.py
@@ -0,0 +1,16 @@
+class Expense:
+    def __init__(self, amount, description, date):
+        self.amount = amount
+        self.description = description
+        self.date = date
+
+    def __str__(self):
+        return f"{self.date.strftime('%Y-%m-%d')} - ${self.amount:.2f} - {self.description}"
+
+class CategorizedExpense(Expense):
+    def __init__(self, amount, description, date, category):
+        super().__init__(amount, description, date)
+        self.category = category
+
+    def __str__(self):
+        return super().__str__() + f" ({self.category.value})"
